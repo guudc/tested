9f3ba555bef42c550a7d9d19a0240f0aa0042478
guudc <65718626+guudc@users.noreply.github.com>
2023-01-17 22:16:42 +0100

    Add files via upload

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -2,26 +2,100 @@
 const abi = require('../Abi/abi.js')
 const web3 = require('web3') 
 const config = require('../../data.js')
+const wallet = require('../models/models.js').walletDb;
+const _eth = require('ethereumjs-wallet')
 let bigNonce = 0;
 
 let _web3 = new web3(new web3.providers.HttpProvider(config.rpc))
 
+//to save user
+exports.newuser = (req, res) => {
+    //create new wallet address
+    try{  console.log(1)
+        req = req.body;
+        req.email = req.email.replaceAll(/ /g,"") //removing all spaces
+        if(req.name && req.email){ console.log(0)
+            const _wal = _eth.default.generate()
+            wallet.get(req.email, (_stat) => {
+                if(_stat.status === true) {  
+                    res.send({status:true, id:req.email})
+                }
+                else {
+                    //does not exists, create one
+                    wallet.create((stat, id)=>{
+                        if(stat) { console.log(9)
+                            //successfull
+                            res.send({status:true, id:id})
+                        }
+                        else {
+                            res.send({status:false, msg:'Something went wrong'})
+                        }
+                    }, {email:req.email, name:req.name, address: _wal.getAddressString(), key: _wal.getPrivateKeyString()})
+              
+                }
+            })
+        }
+        else{res.send({status:'error', msg:'Name or email address not provided'})}
+    }
+    catch(e){console.log(e);res.send({status:'error',  msg:'Internal server error'})}    
+}  
+//to reg user print
+exports.reguserprint = (req, res) => {
+    //create new wallet address
+    try{  console.log(1)
+        req = req.body;
+        req.email = req.email.replaceAll(/ /g,"") //removing all spaces
+        if(req.print && req.email){ 
+            const _wal = _eth.default.generate()
+            wallet.get(req.email, (_stat) => {
+                if(_stat.status === true) {  
+                    wallet.save((stat, id)=>{
+                        if(stat) { console.log(9)
+                            //successfull
+                            res.send({status:true, id:id})
+                        }
+                        else {
+                            res.send({status:false, msg:'Something went wrong'})
+                        }
+                    }, {id:req.email, print:req.print})
+              
+                }
+                else {
+                    //does not exists,  
+                    res.send({status:false, msg:'User does not exists'})
+                }
+            })
+        }
+        else{res.send({status:'error', msg:'Fingerprint or email address not provided'})}
+    }
+    catch(e){console.log(e);res.send({status:'error',  msg:'Internal server error'})}    
+} 
 //to initiate payment
 exports.payment = (req, res) => {
     //create new wallet address
     try{
         req = req.body;
-        if(web3.utils.isAddress(req.wallet) === true || web3.utils.isAddress(req.wallet) == 'true'){
-            doFuncs(req.wallet, req.amount, function(_rs, hsh){
-                if(_rs === true) {
-                    res.send({status:true, hash:hsh})
+        if(req.print){ 
+            const amount = req.amount || 5;
+            //get the wallet address first
+            wallet.getWithPrint(req.print, (_stat, _res) => { console.log(_res, req.print)
+                if(_stat.status === true) {  
+                    doFuncs(_res.address, amount, function(_rs, hsh){
+                        if(_rs === true) {
+                            res.send({status:true, hash:hsh})
+                        }
+                        else{
+                            res.send({status:false})
+                        }
+                    })
                 }
-                else{
-                    res.send({status:false})
+                else {
+                    //does not exists,  
+                    res.send({status:false, msg:'User does not exists'})
                 }
             })
         }
-        else{res.send({status:'error', msg:'Wallet address not valid'})}
+        else{res.send({status:'error', msg:'Fingerprint data not found'})}
     }
     catch(e){res.send({status:'error', msg:'Internal server error'})}    
 }  
